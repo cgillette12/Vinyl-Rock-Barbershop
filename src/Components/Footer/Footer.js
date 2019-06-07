0c759eb5d4927a53ce289f728fe022ee07f64452
Cody Gillette <cgillette1312@gmail.com>
2019-06-06 19:17:59 -0700

    barber list

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,7 +6,7 @@ export default class Footer extends Component {
         return (
                 <div className='footer-container'>
                     <ul className='footer-list'>
-                        <li><h3>ContactInfo</h3></li>
+                        <li><h3>Contact Info</h3></li>
                         <li>Phone#:(916) 632-1083</li>
                         <li>Address: 5975 Pacific St, Rocklin, CA 95677</li>
                     </ul>
--- src/Pages/BarberPage/BarberPage.js
+++ src/Pages/BarberPage/BarberPage.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
 
 import BarberList from '../../Components/BarberList/BarberList'
+import './BarberPage.css'
+
 export default class BarberPage extends Component {
 
-render() {
-    return (
-        <div>
-            <h1>Pick Your Pro</h1>
-            <p> Only next day appointments</p>
-            <BarberList />
-        </div>
-    )
-}
+    render() {
+        return (
+            <div className='Barber-container'>
+                <div className='Barber-list'>
+
+                    <h1>Pick Your Pro</h1>
+                    <p> Only next day appointments</p>
+                    <BarberList />
+                </div>
+            </div>
+        )
+    }
 }
