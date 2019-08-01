847f8acb8e91c76439ced2dfb9e025b5fd4f024f
Cody Gillette <cgillette1312@gmail.com>
2019-07-31 17:39:22 -0700

    fixed it

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-import AuthApiService from '../Services/auth-api-service'
 import TokenService from '../Services/token-service'
-import IdleService from '../Services/Idle-service'
 
 const UserContext = React.createContext({
   user: {},
@@ -9,8 +7,9 @@ const UserContext = React.createContext({
   setError: () => { },
   clearError: () => { },
   setUser: () => { },
+  clearUser: () => { },
   processLogin: () => { },
-  processLogout: () => { },
+  processLogout: () => { }
 })
 
 export default UserContext
@@ -19,33 +18,21 @@ export class UserProvider extends Component {
   constructor(props) {
     super(props)
     const state = { user: {}, error: null }
-
+  
     const jwtPayload = TokenService.parseAuthToken()
 
     if (jwtPayload)
       state.user = {
         id: jwtPayload.user_id,
-        name: jwtPayload.name,
-        username: jwtPayload.sub,
+        user_name: jwtPayload.sub,
+        first_name: jwtPayload.first_name,
+        last_name: jwtPayload.last_name
       }
 
     this.state = state;
-    IdleService.setIdleCallback(this.logoutBecauseIdle)
   }
 
-  componentDidMount() {
-    if (TokenService.hasAuthToken()) {
-      IdleService.regiserIdleTimerResets()
-      TokenService.queueCallbackBeforeExpiry(() => {
-        this.fetchRefreshToken()
-      })
-    }
-  }
-
-  componentWillUnmount() {
-    IdleService.unRegisterIdleResets()
-    TokenService.clearCallbackBeforeExpiry()
-  }
+  
 
   setError = error => {
     console.error(error)
@@ -60,6 +47,11 @@ export class UserProvider extends Component {
     this.setState({ user })
   }
 
+  clearUser = () => {
+    this.setState({ user: {} })
+  }
+
+
   processLogin = authToken => {
     TokenService.saveAuthToken(authToken)
     const jwtPayload = TokenService.parseAuthToken()
@@ -73,38 +65,21 @@ export class UserProvider extends Component {
       phone_number:jwtPayload.phone_number,
       date_created: jwtPayload.date_created
     })
-    IdleService.regiserIdleTimerResets()
-    TokenService.queueCallbackBeforeExpiry(() => {
-      this.fetchRefreshToken()
-    })
   }
 
   processLogout = () => {
     TokenService.clearAuthToken()
     TokenService.clearCallbackBeforeExpiry()
-    IdleService.unRegisterIdleResets()
     this.setUser({})
   }
 
   logoutBecauseIdle = () => {
     TokenService.clearAuthToken()
     TokenService.clearCallbackBeforeExpiry()
-    IdleService.unRegisterIdleResets()
     this.setUser({ idle: true })
   }
 
-  fetchRefreshToken = () => {
-    AuthApiService.refreshToken()
-      .then(res => {
-        TokenService.saveAuthToken(res.authToken)
-        TokenService.queueCallbackBeforeExpiry(() => {
-          this.fetchRefreshToken()
-        })
-      })
-      .catch(err => {
-        this.setError(err)
-      })
-  }
+  
 
   render() {
     const value = {
@@ -113,8 +88,10 @@ export class UserProvider extends Component {
       setError: this.setError,
       clearError: this.clearError,
       setUser: this.setUser,
+      clearUser: this.clearUser,
       processLogin: this.processLogin,
-      processLogout: this.processLogout,
+      processLogout: this.processLogout
+
     }
     return (
       <UserContext.Provider value={value}>
--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import { BarberListProvider } from './Contexts/BarberListContext'
+import { UserProvider } from './Contexts/UserContext'
 import App from './Components/App/App';
 import './index.css';
 
 
 ReactDOM.render(
     <BrowserRouter>
-        <BarberListProvider>
-                <App />            
-        </BarberListProvider>
+        <UserProvider>
+            <BarberListProvider>
+                <App />
+            </BarberListProvider>
+        </UserProvider>
     </BrowserRouter>
     , document.getElementById('root'));
