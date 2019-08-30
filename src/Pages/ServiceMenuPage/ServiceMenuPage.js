22d4c2520dcfff69e44ddab4efcc85dec6c401a9
Cody Gillette <cgillette1312@gmail.com>
2019-08-30 15:59:59 -0700

    updated

--- src/Pages/ServiceMenuPage/ServiceMenuPage.js
+++ src/Pages/ServiceMenuPage/ServiceMenuPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import BarberApiService from '../../Services/barber-api-service'
-import { ServiceList } from '../../Components/Utilitys/Utils'
+import { ServiceList, Spinner } from '../../Components/Utilitys/Utils'
 import './ServiceMenuPage.css'
 
 export default class ServiceMenuPage extends Component {
@@ -12,28 +12,34 @@ export default class ServiceMenuPage extends Component {
     componentDidMount() {
         BarberApiService.getBarberServices()
             .then(services => {
-                return this.setState({
+                return  this.setState({
                     barberServices: services,
                     loader: false
                 })
-
             })
+                .catch(() => {
+                    this.setState({
+                        loader:true,
+                        error:'there was an error'
+                    })
+                    this.wait(5000)
+                })
     }
     sleep = milliseconds => {
         return new Promise(resolve => setTimeout(resolve, milliseconds));
     };
     wait = async (milliseconds = 2000) => {
         await this.sleep(milliseconds);
+        return this.state.error
     };
     render() {
-        const {barberServices,loader} = this.state
         return (
             <div className='ServiceMenu-container'>
                 <section className='Service-menu-list'>
                 <h1> Service Menu </h1>
-                <ServiceList 
-                    barberServices={barberServices} 
-                    loader={loader}/>
+                    {this.state.error ?
+                    <Spinner error={this.state.error}/> :
+              <ServiceList {...this.state}/>}
                 </section>
             </div>
         )
